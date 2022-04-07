e4a03626454387cfc8db2cfaaf7bb968b851834b
hodujwan <101338760+hodujwan@users.noreply.github.com>
2022-04-07 11:00:46 +0900

    Update PushPrint.java
    PushPrint.java modified on GitHub

--- show/src/show/PushPrint.java
+++ show/src/show/PushPrint.java
@@ -3,7 +3,8 @@ package show;
 public class PushPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Add Java file");
+		System.out.println("Hello...");
 
 	}
 
